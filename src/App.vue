aba497e1d69b1b3ad5885d28831d4919519ce816
MrJanHorak <janny_b_goode@hotmail.com>
2023-03-13 23:58:12 -0400

    create a vue todo app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="appContainer">
+    <h1>To-Do List</h1>
+
+    <input
+      v-model="inputValue"
+      placeholder="Enter a new task"
+      v-on:keyup.enter="onSubmit"
+    />
+
+    <ListItem :list="taskArray" />
+    <img src="./assets/logo.png" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/ListItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListItem
+  },
+  data: () => ({
+    inputValue: '',
+    index: 0,
+    taskArray: []
+  }),
+  methods: {
+    onSubmit() {
+      if (this.inputValue === '') {
+        return
+      }
+      console.log('I am submitting: ', this.inputValue)
+      this.taskArray.push({ item: this.inputValue, id: this.index })
+      console.log(this.taskArray)
+      this.index++
+      this.inputValue = ''
+    }
   }
 }
 </script>
 
 <style>
+html {
+  background: linear-gradient(
+    22deg,
+    rgba(19, 146, 235, 1) 0%,
+    rgba(8, 228, 196, 1) 100%
+  );
+  background-attachment: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +62,42 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: white;
+  border-radius: 15px;
+  width: 70vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 4px 4px 4px, rgba(60, 64, 67, 0.15) 0px 5px 8px 8px;
+}
+
+img {
+  width: 77px;
+  height: auto;
+  margin: 25px;
+}
+
+input {
+  width: 58vw;
+  height: 55px;
+  border-radius: 8px;
+  border: 0.5px solid grey;
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: 8px;
+  padding-left: 18px;
+}
+
+input::placeholder {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.appContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="listItem  "
+    :class="{isComplete}"
+    v-on:click="isComplete=!isComplete"
+    :key="task.id"
+    v-for="task, in list"
+  >
+  <!-- <div class="taskRow"> -->
+    <h2 ><span v-if="!isComplete">&ensp;</span><span v-if="isComplete">&#x2713;</span>{{ task.item }}</h2>
+  <!-- </div> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ListItem',
+  props: ['list'],
+  data:()=>({
+    isComplete: false
+  })
+}
+</script>
+
+<style>
+  h2 {
+    margin-left: 48px;
+  }
+
+.listItem {
+  color: gray;
+  border: solid 2px gray;
+  border-radius: 15px;
+  width: 60vw;
+  text-align: left;
+  margin: 12px;
+}
+
+.isComplete {
+  color: green;
+  text-decoration: line-through;
+}
+
+/* .taskRow {
+  display: flex;
+  flex-direction: row;
+} */
+
+</style>
